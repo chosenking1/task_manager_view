215b2d52d3ded4b5a6e219aedcffbd06987b56fe
chosenking1 <82238206+chosenking1@users.noreply.github.com>
2023-05-30 14:57:32 +0100

    Made an update

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,3 +1,4 @@
-const apiUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : 'http://localhost:8000';
-
+require('dotenv').config();
+// const apiUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : 'http://localhost:8000';
+const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 export default apiUrl;
