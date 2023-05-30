305c732ebe9a92604a5baa101826ce1e1a3c6ed4
chosenking1 <82238206+chosenking1@users.noreply.github.com>
2023-05-30 20:22:11 +0100

    Made an update

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,7 @@
-const apiUrl = process.env.NODE_ENV === 'production'
-  ? 'https://task-manager-api-production-e771.up.railway.app'
-  : 'http://localhost:8000';
+// const apiUrl = process.env.NODE_ENV === 'production'
+//   ? 'https://task-manager-api-production-e771.up.railway.app'
+//   : 'http://localhost:8000';
 
+const apiUrl = 'https://task-manager-api-production-e771.up.railway.app'
 export default apiUrl;
 
