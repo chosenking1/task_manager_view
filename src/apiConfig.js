cecbbd61e2b26f54682a18686e5f1fad0b6e9d72
chosenking1 <82238206+chosenking1@users.noreply.github.com>
2023-05-30 20:09:56 +0100

    Made an update

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
-// const apiUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : 'http://localhost:8000';
-const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+const apiUrl = process.env.NODE_ENV === 'production'
+  ? 'https://task-manager-api-production-e771.up.railway.app'
+  : 'http://localhost:8000';
+
 export default apiUrl;
+
