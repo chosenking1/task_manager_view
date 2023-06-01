c6ae2a9a651d7a03ad1d99a70dc4c5d9813aecd4
chosenking1 <82238206+chosenking1@users.noreply.github.com>
2023-06-01 18:30:01 +0100

    Made an update

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -48,9 +48,9 @@ const Navbar = () => {
   };
 
   return (
-    <div className="shadow-md w-full fixed top-0 left-0 z-10">
+      <div className="shadow-md w-full fixed top-0 left-0">
       <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
-        <div className="flex items-center">
+
           <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800">
             <span className="text-3xl text-indigo-600 mr-1 pt-2">
               <ion-icon name="logo-ionic"></ion-icon>
@@ -58,55 +58,57 @@ const Navbar = () => {
             Designer
           </div>
 
-          {isLoggedIn && (
-            <ul className="bg:green ml-4 hidden md:flex">
-              {Links.map((link) => (
-                <li key={link.name} className="text-xl my-1">
-                  <a
-                    href={link.link}
-                    className="text-gray-800 hover:text-gray-400 duration-500"
-                  >
-                    {link.name}
-                  </a>
-                </li>
-              ))}
-              <li>
-                <button
-                  className="bg-indigo-600 text-white font-[Poppins] py-2 px-6 rounded hover:bg-indigo-400 duration-500"
-                  onClick={handleLogout}
-                >
-                  Logout
-                </button>
-              </li>
-            </ul>
-          )}
-        </div>
+          {/*{isLoggedIn && (*/}
+          {/*  <ul className="bg-green ml-4 hidden md:flex space-x-4">*/}
+          {/*    {Links.map((link) => (*/}
+          {/*      <li key={link.name} className="text-xl">*/}
+          {/*        <a*/}
+          {/*          href={link.link}*/}
+          {/*          className="text-blue-500 hover:text-gradient-blue-black duration-500"*/}
+          {/*        >*/}
+          {/*          {link.name}*/}
+          {/*        </a>*/}
+          {/*      </li>*/}
+          {/*    ))}*/}
+          {/*    <li>*/}
+          {/*      <button*/}
+          {/*        className="bg-indigo-600 text-white font-[Poppins] py-2 px-6 rounded hover:bg-indigo-400 duration-500"*/}
+          {/*        onClick={handleLogout}*/}
+          {/*      >*/}
+          {/*        Logout*/}
+          {/*      </button>*/}
+          {/*    </li>*/}
+          {/*  </ul>*/}
+          {/*)}*/}
+
 
         <div
           onClick={() => setOpen(!open)}
-          className="text-3xl cursor-pointer md:hidden"
+          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
         >
           <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
         </div>
 
-        <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in top-20 ${
-            open ? 'opacity-100 visible' : 'opacity-0 invisible'
-          }`}
-        >
+        {/*<ul*/}
+        {/*  className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in top-20 ${*/}
+        {/*    open ? 'opacity-100 visible' : 'opacity-0 invisible'*/}
+        {/*  }`}*/}
+        {/*>*/}
+         <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
           {isLoggedIn &&
             Links.map((link) => (
               <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
                 <a
                   href={link.link}
-                  className="text-gray-800 hover:text-gray-400 duration-500"
+                  className="text-blue-950 hover:text-emerald-300 hover:underline  duration-500"
                 >
                   {link.name}
                 </a>
               </li>
-            ))}
+            ))
+          }
           {isLoggedIn && (
-            <li className="md:ml-8 text-xl md:my-0 my-7">
+            <li className="md:my-2">
               <button
                 className="bg-indigo-600 text-white font-[Poppins] py-2 px-6 rounded md:ml-8 hover:bg-indigo-400 duration-500"
                 onClick={handleLogout}
@@ -118,6 +120,7 @@ const Navbar = () => {
         </ul>
       </div>
     </div>
+
   );
 };
 
